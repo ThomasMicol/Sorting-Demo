e6f561acd917d2b86635d9683124a82160c14665
Thomas Micol <thomasmicol@gmail.com>
2020-12-09 22:00:55 +1300

    implemented the sort function

--- src/app.js
+++ src/app.js
@@ -6,8 +6,18 @@ function AddElement() {
     console.log(itemList);
 }
 
-function Sort() {
+function Sort(array, start, end) {
+    if (start >= end) {
+        return;
+    }
 
+    let partitionIndex = GetPartitionIndex(array, start, end);
+    Sort(array, start, partitionIndex - 1);
+    Sort(array, index + 1, end);
+}
+
+function GetPartitionIndex(array, start, end){
+    
 }
 
 function Swap(array, indexFrom, indexTo) {
